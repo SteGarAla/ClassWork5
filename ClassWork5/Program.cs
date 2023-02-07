413d098e3e04d39b84162be3ae527f975d614527
SteGarAla <stegarc@ut.utm.edu>
2023-02-07 11:19:31 -0600

    Add project files.

---
+++ ClassWork5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
